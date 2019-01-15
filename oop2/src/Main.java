9d754b90ac005212fdb82a2ec4468ad27fa2566e
pkijak <46262684+pkijak@users.noreply.github.com>
2019-01-15 11:42:00 +0100

    Kompozycje, podstawy

--- oop2/src/Main.java
+++ oop2/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        Motherboard motherboard = new Motherboard("Super", "Lenovo", 5, 2, "Updated");
+        Monitor monitor = new Monitor("R20", "LG", 21, new Resolution(1920, 1080));
+        Case theCase = new Case("Testowy", "chujwie", "Mocny", new Dimensions(50, 80, 30));
+
+        PC komputer = new PC(theCase, monitor, motherboard);
+
+        komputer.getMonitor().drawPixelAt(50,30,"red");
+        komputer.getMotherboard().loadPorgram("testowy");
+        komputer.getTheCase().pressPowerButton();
     }
 }
