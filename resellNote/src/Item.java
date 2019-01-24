8a9cc802dc36622e0475fb64ecaccc0ec0d1b018
pkijak <46262684+pkijak@users.noreply.github.com>
2019-01-24 13:47:54 +0100

    resell note test

--- resellNote/src/Item.java
+++ resellNote/src/Item.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Item {
+
+    private String nazwa;
+    private int cena;
+    private int stan;
+
+    public Item(String nazwa, int cena, int stan) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.stan = stan;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public int getCena() {
+        return cena;
+    }
+
+    public int getStan() {
+        return stan;
+    }
 }
--- resellNote/src/menu.java
+++ resellNote/src/menu.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class menu {
+
+    private static Storage magazyn;
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void load() {
+        System.out.println("Dostepne opcje: \n");
+        System.out.println("1. Sprawdz liste posiadanych przedmiotow\n" +
+                "2. Sprawdz liste sprzedanych przedmiotow\n" +
+                "3. Dodaj przedmiot \n" +
+                "4. Oznacz przedmiot jako sprzedany \n" +
+                "5. Wykaz pieniezny");
+
+        options();
+    }
+
+    public static void options() {
+
+        System.out.println("Wybierz opcje: ");
+        int choice = in.nextInt();
+        while (choice != 9) {
+            switch (choice) {
+                case 1:
+                    magazyn.show();
+                    break;
+                case 2:
+                    //sold;
+                    break;
+                case 3:
+                    magazyn.add();
+                    break;
+                case 4:
+                    magazyn.itemInfo();
+                case 5:
+                    // money wykaz
+            }
+            load();
+        }
+
+    }
 }
